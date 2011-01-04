1cb20045b4c549fea403f2950b409bd262025c47
Gurtej Kanwar <gurtejkanwar@gmail.com>
2011-01-04 16:16:00 +0000

    Reworking the collision mechanism a bit to add a few features, make things easier to code, and possibly increase collision processing speed.

---
+++ CollisionRework/bin/ndk/local/armeabi/objs/thelements/app-android.o.d
@@ -0,0 +1,182 @@
+/home/looterguf/workspace/TheElements/bin/ndk/local/armeabi/objs/thelements/app-android.o: \
+ /home/looterguf/workspace/TheElements/jni/app-android.c \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/jni.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/math.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/limits.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/limits.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/limits.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/internal_types.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/limits.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdlib.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/string.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/malloc.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/alloca.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/strings.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/memory.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdio.h \
+ /home/looterguf/workspace/TheElements/jni/app.h \
+ /home/looterguf/workspace/TheElements/jni/macros.h \
+ /home/looterguf/workspace/TheElements/jni/setup.h \
+ /home/looterguf/workspace/TheElements/jni/elementproperties.h \
+ /home/looterguf/workspace/TheElements/jni/collisions.h \
+ /home/looterguf/workspace/TheElements/jni/saveload.h \
+ /home/looterguf/workspace/TheElements/jni/points.h \
+ /home/looterguf/workspace/TheElements/jni/server.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/socket.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/socket.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/socket.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/sockios.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/sockios.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/uio.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netinet/in.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/endian.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/endian.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/in.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/byteorder.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/swab.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/generic.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/in6.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netinet/in6.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netdb.h \
+ /home/looterguf/workspace/TheElements/jni/gl.h \
+ /home/looterguf/workspace/TheElements/jni/importgl.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h \
+ /home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/looterguf/workspace/TheElements/jni/update.h \
+ /home/looterguf/workspace/TheElements/jni/collide.h
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/jni.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/math.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/limits.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/limits.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/limits.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/internal_types.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/machine/limits.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdlib.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/string.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/malloc.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/alloca.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/strings.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/memory.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/stdio.h:
+
+/home/looterguf/workspace/TheElements/jni/app.h:
+
+/home/looterguf/workspace/TheElements/jni/macros.h:
+
+/home/looterguf/workspace/TheElements/jni/setup.h:
+
+/home/looterguf/workspace/TheElements/jni/elementproperties.h:
+
+/home/looterguf/workspace/TheElements/jni/collisions.h:
+
+/home/looterguf/workspace/TheElements/jni/saveload.h:
+
+/home/looterguf/workspace/TheElements/jni/points.h:
+
+/home/looterguf/workspace/TheElements/jni/server.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/socket.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/socket.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/socket.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/sockios.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/sockios.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/uio.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netinet/in.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/endian.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/sys/endian.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/in.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/asm/byteorder.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/linux/in6.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netinet/in6.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/netdb.h:
+
+/home/looterguf/workspace/TheElements/jni/gl.h:
+
+/home/looterguf/workspace/TheElements/jni/importgl.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h:
+
+/home/looterguf/android-ndk-r4/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/looterguf/workspace/TheElements/jni/update.h:
+
+/home/looterguf/workspace/TheElements/jni/collide.h:
---
+++ CollisionRework/src/sand/falling/opengl/preferences/PreferencesFromCode.java
@@ -0,0 +1,206 @@
+package sand.falling.opengl.preferences;
+
+
+import sand.falling.opengl.MainActivity;
+import sand.falling.opengl.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class PreferencesFromCode extends PreferenceActivity implements OnSharedPreferenceChangeListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setPreferenceScreen(createPreferenceHierarchy());
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private PreferenceScreen createPreferenceHierarchy()
+    {
+        // Root
+        PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+        
+        // Inline preferences 
+        PreferenceCategory inlinePrefCat = new PreferenceCategory(this);
+        inlinePrefCat.setTitle("Look and Feel");
+        root.addPreference(inlinePrefCat);
+        
+       
+        
+        // List preference
+        ListPreference listPref = new ListPreference(this);
+        listPref.setEntries(R.array.entries_list_preference);
+        listPref.setEntryValues(R.array.entryvalues_list_preference);
+        listPref.setDialogTitle("Background");
+        listPref.setKey("background_color");
+        listPref.setTitle("Background Color");
+        listPref.setSummary("Sets the color of the background");
+        
+        root.addPreference(listPref);
+
+       
+        PreferenceCategory launchPrefCat = new PreferenceCategory(this);
+        launchPrefCat.setTitle("Other");
+        root.addPreference(launchPrefCat);
+        
+        CheckBoxPreference togglePref = new CheckBoxPreference(this);
+        togglePref.setKey("flip_settings");
+        togglePref.setTitle("Flip Screen");
+        togglePref.setSummary("Flip Screen (Use only if nothing draws on screen)");
+        root.addPreference(togglePref);
+        
+        CheckBoxPreference sleep_on_off = new CheckBoxPreference(this);
+        sleep_on_off.setKey("sleep");
+        sleep_on_off.setTitle("Sleep On/Off");
+        sleep_on_off.setSummary("Allow the screen to sleep during play or not");
+        root.addPreference(sleep_on_off);
+        
+        CheckBoxPreference ui_on_off = new CheckBoxPreference(this);
+        ui_on_off.setKey("ui");
+        ui_on_off.setTitle("UI On/Off");
+        ui_on_off.setSummary("Turn the UI on or off");
+        root.addPreference(ui_on_off);
+        
+        PreferenceCategory game = new PreferenceCategory(this);
+        game.setTitle("Game");
+        root.addPreference(game);
+        
+        CheckBoxPreference acceler = new CheckBoxPreference(this);
+        acceler.setKey("accel_control");
+        acceler.setTitle("Accelerometer Control");
+        acceler.setSummary("Turn accelerometer control of gravity on/off ");
+        root.addPreference(acceler);
+        
+        
+        final SharedPreferences sharedPrefs =
+            PreferenceManager.getDefaultSharedPreferences(this);
+        if (!sharedPrefs.contains("background_color"))
+        {
+        	listPref.setValue("black");
+        }
+        if (!sharedPrefs.contains("flip_settings"))
+        {
+        	togglePref.setChecked(false);        	
+        }
+        if (!sharedPrefs.contains("ui"))
+        {
+        	ui_on_off.setChecked(true);
+        }
+        if (!sharedPrefs.contains("sleep"))
+        {
+        	sleep_on_off.setChecked(true);
+        }
+        if (!sharedPrefs.contains("accel_control"))
+        {
+        	acceler.setChecked(false);
+        }
+        
+        return root;
+    }
+    
+    //When something is clicked, this finds it and calls a JNI function to change it in the c code
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        // Let's do something when my counter preference value changes
+        //Log.v("DemoActivity", "onpref called");
+        if (key == "background_color")
+        {
+        	//Log.v("DemoActivity", "onpref color: " + sharedPreferences.getString(key, "black"));
+        	if (sharedPreferences.getString( key, "black").equals("black"))
+        	{
+        		MainActivity.setBackgroundColor(0);
+        	}
+        	else
+        	{
+        		MainActivity.setBackgroundColor(1);
+        	}
+        }
+        else if (key == "flip_settings")
+        {
+        	Log.v("DemoActivity", "onpref flip");
+        	if ( sharedPreferences.getBoolean(key, false) == true)
+        	{
+        		MainActivity.setFlip(1);
+        	}
+        	else
+        	{
+        		MainActivity.setFlip(0);
+        	}
+        }
+        else if (key == "ui")
+        {
+        	MainActivity.ui = sharedPreferences.getBoolean(key, true);
+        	Toast.makeText(this, "UI changed: please restart for effect", Toast.LENGTH_LONG).show();
+        }
+        else if (key == "sleep")
+        {
+        	MainActivity.sand_view.setKeepScreenOn(sharedPreferences.getBoolean(key,true));
+        }
+        else if (key == "accel_control")
+        {
+        	if ( sharedPreferences.getBoolean(key,false) == true){
+        		MainActivity.setAccelOnOff(1);
+        	}
+        	else{
+        		MainActivity.setAccelOnOff(0);
+        	}
+        }
+    }
+    //used to set stuff up when the app opens and retrieve the settings
+    public static void setpreferences(Context context)
+    {
+        final SharedPreferences sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(context);
+    	if (sharedPreferences.getString("background_color", "black").equals("black"))
+    	{
+    		MainActivity.setBackgroundColor(0);
+    	}
+    	else
+    	{  
+    		MainActivity.setBackgroundColor(1);
+    	}
+    	if (sharedPreferences.getBoolean("flip_settings", false) == true) //flip screen stuff
+    	{
+    		MainActivity.setFlip(1);
+    	}
+    	else
+    	{
+    		MainActivity.setFlip(0);
+    	}
+    	if (sharedPreferences.getBoolean("accel_control", false) == true) //flip screen stuff
+    	{
+    		MainActivity.setAccelOnOff(1);
+    	}
+    	else
+    	{
+    		MainActivity.setAccelOnOff(0);
+    	}
+    	
+ 
+    	
+    	//Set the bool variable ui in the Activity
+    	MainActivity.ui = sharedPreferences.getBoolean("ui", true);
+    }
+    public static void setScreenOnOff(Context context) //set screen on or off, needs to be seperate because sand_view is created after setPrefs is called
+    {
+    	final SharedPreferences sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(context);
+    	
+    	MainActivity.sand_view.setKeepScreenOn(sharedPreferences.getBoolean("screen",true)); //keep screen on or not
+    	
+    }
+}
