22f993dc29ebbd1cb46d422a4b64eebe4c8ad2fa
Gurtej Kanwar <gurtejkanwar@gmail.com>
2012-06-14 02:05:21 +0000

    Further develops the Custom Elements UI.
    I need the specific file format to proceed much further.

---
+++ src/com/idkjava/thelements/custom/CustomElementManager.java
@@ -0,0 +1,30 @@
+package com.idkjava.thelements.custom;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import com.idkjava.thelements.game.FileManager;
+
+import android.util.Log;
+
+public class CustomElementManager
+{
+	private static ArrayList<CustomElement> sCustomElements = new ArrayList<CustomElement>();
+	private static final File elementDir = new File(FileManager.ROOT_DIR + FileManager.ELEMENTS_DIR);
+	
+	public static void refresh()
+	{
+		//Get the array of filenames
+		String[] elementFiles = elementDir.list();
+		Log.v("TheElements", "CustomElementManager refreshed, files found: " + elementFiles.length);
+		for(int i = 0; i < elementFiles.length; i++)
+		{
+			Log.v("TheElements", "..." + elementFiles[i]);
+			sCustomElements.add(new CustomElement(elementFiles[i]));
+		}
+	}
+	public static ArrayList<CustomElement> getElementList()
+	{
+		return sCustomElements;
+	}
+}
--- src/com/idkjava/thelements/custom/CustomElementManagerActivity.java
+++ src/com/idkjava/thelements/custom/CustomElementManagerActivity.java
@@ -1,11 +1,10 @@
 package com.idkjava.thelements.custom;
 
 import com.idkjava.thelements.R;
-import com.idkjava.thelements.game.SaveManager;
+import com.idkjava.thelements.game.FileManager;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -34,30 +33,31 @@ public class CustomElementManagerActivity extends Activity
 	private static TableRow tr;
 	private static LinearLayout buttonContainer;
 	private static Resources res;
-	List<String> elementList = new ArrayList<String>();
-	CustomElementArrayAdapter arrayAdapter;
 	
 	
-	//TODO(MASSIVE,ARMORED): This should display all loaded Custom Elements and allow selection,
-	//editing, and creation
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.custom_element_manager_activity);
+		res = getResources();
 	}
 	@Override
 	protected void onResume()
 	{
 		super.onResume();
-		refreshElements();
+		CustomElementManager.refresh();
+		ArrayList<CustomElement> elements = CustomElementManager.getElementList();
 		
-		//Go through and find all the save files and dynamically load them
-		int length = elementList.size();
+		//Go through and find all the save files and dynamically add them
+		int length = elements.size();
 		if(length != 0)
 		{
 			for(int i = 0; i < length; i++)
 			{
-				addEntity(elementList.get(i));
+				if (elements.get(i).isValid())
+				{
+					addEntity(elements.get(i).getName(), elements.get(i).getFilename());
+				}
 			}
 		}
 		else
@@ -74,37 +74,10 @@ public class CustomElementManagerActivity extends Activity
 			tl.addView(tr);
 		}
 	}
-	private void refreshElements()
-	{
-		elementList.clear();
-		File file = new File("/sdcard/thelements/elements/");
-		if(file.exists())
-		{
-			File[] elementFiles = file.listFiles();
-			for(int i = 0; i < elementFiles.length; i++)
-			{
-				loadCustomElement(elementFiles[i]);
-			}
-		}
-		else
-		{
-			file.mkdirs();
-		}
-		//arrayAdapter.notifyDataSetChanged();
-		
-		// Temporarily load some fake elements
-		elementList.add("Hello");
-		elementList.add("Element1");
-		elementList.add("Element2");
-		
-		
-	}
 	
-    public void addEntity(String entityName)
+    public void addEntity(String elementName, String filename)
     {
-    	final String entityNameFinal = entityName;
-        //DIP representation math
-        //final float SCALE = getBaseContext().getResources().getDisplayMetrics().density;
+    	final String filenameFinal = filename;
         
         //Create a TableLayout object associated with the TableLayout in the .xml file
         TableLayout tl = (TableLayout)findViewById(R.id.loads_container);
@@ -118,22 +91,23 @@ public class CustomElementManagerActivity extends Activity
 		buttonContainer.setPadding(25, 10, 25, 10);
 		buttonContainer.setGravity(Gravity.RIGHT);
 		
-		//Create a TextView to hold the filename
-		TextView elementname = new TextView(this);
-		elementname.setText(entityName);
-		elementname.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
+		//Create a TextView to hold the element name
+		TextView nameTextView = new TextView(this);
+		// TODO: Change this to elementName when loading works
+		nameTextView.setText(filename);
+		nameTextView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
 		//filename.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
 		
 		//Makes use of DisplayMetrics to create an automatic dip unit :)
 		//filename.setPadding((int)(16 * SCALE + 0.5f), 0, 0, 0);
 		//Add the TextView to the TableRow
-		buttonContainer.addView(elementname);
+		buttonContainer.addView(nameTextView);
 
 		//Set buttonContainer's gravity to right with a 16dip right padding
 		//buttonContainer.setPadding(0, 0, (int)(16 * SCALE + 0.5f), 0);
 		//buttonContainer.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, 50));
 		
-        //Create a button to be the action invoker
+        // Edit element button
         actionButton = new ImageButton(this);
         actionButton.setBackgroundResource(R.drawable.load_state_select);
         actionButton.setLayoutParams(new LayoutParams(
@@ -146,7 +120,7 @@ public class CustomElementManagerActivity extends Activity
     			public void onClick(View v)
     			{
     				Intent i = new Intent(CustomElementManagerActivity.this, CustomElementActivity.class);
-    				i.putExtra("elementname", entityNameFinal);
+    				i.putExtra("filename", filenameFinal);
     				startActivity(i);
                 	finish();
     			}
@@ -154,6 +128,7 @@ public class CustomElementManagerActivity extends Activity
         );
         buttonContainer.addView(actionButton);
         
+        // Delete button
         actionButton = new ImageButton(this);
         actionButton.setBackgroundResource(R.drawable.load_state_delete);
         actionButton.setLayoutParams(new LayoutParams(
@@ -165,13 +140,15 @@ public class CustomElementManagerActivity extends Activity
 			{
 				public void onClick(View v)
 				{
-					SaveManager.deleteState(entityNameFinal);
+					// Delete the custom element file
+					(new File(FileManager.ROOT_DIR + FileManager.ELEMENTS_DIR + filenameFinal)).delete();
+					
+					// Update the view accordingly
 					ViewGroup parent = (ViewGroup) v.getParent().getParent();
 					parent.removeView((View) v.getParent());
-					SaveManager.refresh();
-					if(SaveManager.getNumSaves() == 0)
+					TableLayout tl = (TableLayout)findViewById(R.id.loads_container);
+					if(tl.getChildCount() == 0)
 					{
-						TableLayout tl = (TableLayout)findViewById(R.id.loads_container);
 						tr = new TableRow(getBaseContext());
 						tr.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 						tr.setGravity(Gravity.CENTER);
@@ -187,34 +164,24 @@ public class CustomElementManagerActivity extends Activity
 		);
 		buttonContainer.addView(actionButton);
         
-        //Add a test click event
+        // Click on the entire row
         buttonContainer.setOnClickListener
         (
         	new OnClickListener()
 	        {
                 public void onClick(View viewParam)
                 {
-                	SaveManager.loadState(entityNameFinal);
+    				Intent i = new Intent(CustomElementManagerActivity.this, CustomElementActivity.class);
+    				i.putExtra("filename", filenameFinal);
+    				startActivity(i);
                 	finish();
                 }
 	        }
         );
         
-        //Add the created row to our TableLayout
+        // Add the created row to our TableLayout
         tl.addView(buttonContainer, new LayoutParams(
                 LayoutParams.FILL_PARENT, 
                 LayoutParams.WRAP_CONTENT));
     }
-	
-	private void loadCustomElement(File file)
-	{
-		// TODO: Make a native call
-	}
-	private void deleteFile(CustomElement customElement)
-	{
-		File file = new File("/sdcard/thelements/elements/" + customElement.getName());
-		if(file.exists())
-			file.delete();
-		refreshElements();
-	}
 }
