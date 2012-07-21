eeac848b1245a1c6661bd1c974ddefe17f5ecf74
Gurtej Kanwar <gurtejkanwar@gmail.com>
2012-07-21 04:02:42 +0000

    Adds elements: - Wood - Termite - Insect killer
    Also, minor bug fixes

---
+++ TheElements-dev/jni/collide.c
@@ -0,0 +1,245 @@
+/*
+ * collide.c
+ * ----------------------------
+ * Defines the collide function, which is the
+ * heart of our app. It processes all the collisions by
+ * type number. It alone is several hundred lines
+ * long, thus the need for a separate file.
+ */
+
+#include "collide.h"
+
+void collide(struct Particle* firstParticle, struct Particle* secondParticle)
+{
+    //Temporary variables
+    int oldXFirst = firstParticle->oldX, oldYFirst = firstParticle->oldY;
+    //The type of the collision (retrieved from a static array)
+
+    int fs = firstParticle->element->index;
+    int ss = secondParticle->element->index;
+    int type;
+    if (fs >= NUM_BASE_ELEMENTS && ss >= NUM_BASE_ELEMENTS)
+    {
+    	type = elements[fs]->collisions[elements[ss]->base];
+    }
+    else if ( fs >= NUM_BASE_ELEMENTS)
+    {
+    	type = elements[fs]->collisions[ss];
+    }
+    else if ( ss >= NUM_BASE_ELEMENTS)
+    {
+    	type = reciprocals[elements[ss]->collisions[fs]];
+    }
+    else
+    {
+    	type = collision[firstParticle->element->index][secondParticle->element->index];
+    }
+
+    switch(type)
+    {
+    case 0: //Solid - Solid
+    {
+	//First particle goes back to where it was before
+	firstParticle->x = oldXFirst;
+	firstParticle->y = oldYFirst;
+	firstParticle->hasMoved = FALSE;
+
+	firstParticle->xVel = (rand() % 3) - 1;
+	break;
+    }
+    case 1: //Density Based
+    {
+        //The denser element replaces the less dense element
+        if (firstParticle->element->density > secondParticle->element->density)
+        {
+            secondParticle->x = oldXFirst;
+            secondParticle->y = oldYFirst;
+            secondParticle->hasMoved = TRUE;
+
+            break;
+        }
+        //The less dense element bounces back and adds some velocities
+        else
+        {
+            firstParticle->x = oldXFirst;
+            firstParticle->y = oldYFirst;
+            firstParticle->hasMoved = FALSE;
+                        
+            //-1 to 1
+            int random = rand() % 2 - 1;
+            firstParticle->yVel = random;
+
+            //-5 to 5
+            random = rand() % 11 - 5;
+            firstParticle->xVel = random;
+
+            break;
+        }
+    }
+    case 2: //Anything - Generator collision
+    {
+        //Change the generator to spawner
+        setElement(secondParticle, elements[SPAWN_ELEMENT]);
+        setParticleSpecialVal(secondParticle, SPECIAL_SPAWN, firstParticle->element->index);
+        secondParticle->hasMoved = TRUE;
+
+        //Move back and delete firstParticle
+        firstParticle->x = oldXFirst;
+        firstParticle->y = oldYFirst;
+        deletePoint(firstParticle);
+        firstParticle->hasMoved = FALSE;
+
+        break;
+    }
+    case 3: //Generator - Anything collision 
+        //(should not be needed, but just in case we have moving generators in the future)
+    {
+        //Change the generator to spawner
+        setElement(firstParticle, elements[SPAWN_ELEMENT]);
+        setParticleSpecialVal(firstParticle, SPECIAL_SPAWN, secondParticle->element->index);
+        firstParticle->hasMoved = TRUE;
+
+        //Delete secondParticle
+        unSetPoint(secondParticle);
+        secondParticle->hasMoved = FALSE;
+
+        break;
+    }
+    case 4: //Acid - Meltable
+    {
+        //Define some temporary variables
+        int tempX = firstParticle->x, tempY = firstParticle->y;
+        if (rand() % 3 != 0 ) //2/3 chance
+        {
+            //Acid burns away Meltable
+            unSetPoint(secondParticle);
+        }
+        else if (rand() % 2 == 0 ) //Otherwise, 1/6 total
+        {
+            //Acid is neutralized
+            //Move firstParticle back and delete it
+            firstParticle->x = oldXFirst;
+            firstParticle->y = oldYFirst;
+            deletePoint(firstParticle);
+            firstParticle->hasMoved = FALSE;
+        }
+		else //Otherwise, 1/6 total
+		{
+			//Acid bounces
+			firstParticle->x = oldXFirst;
+			firstParticle->y = oldYFirst;
+			firstParticle->hasMoved = FALSE;
+		}
+
+	break;
+    }
+    case 5: //Meltable - Acid
+    {
+	if (rand() % 3 != 0 ) //2/3 chance
+	{
+	    //Meltable is destroyed
+
+	    //Delete firstParticle
+	    firstParticle->x = oldXFirst;
+	    firstParticle->y = oldYFirst;
+	    deletePoint(firstParticle);
+	    firstParticle->hasMoved = FALSE;
+	}
+	else if (rand() % 2 == 0 ) //Otherwise, 1/6 totaln
+	{
+	    //Acid is neutralized
+
+	    //Delete secondParticle
+	    unSetPoint(secondParticle);
+	    secondParticle->hasMoved = FALSE;
+	}
+	else //Otherwise, 1/6 total
+	{
+	    //Meltable bounces
+	    firstParticle->x = oldXFirst;
+	    firstParticle->y = oldYFirst;
+	    firstParticle->hasMoved = FALSE;
+	}
+	break;
+    }
+    case 6: //Acid - Neutralizer
+    {
+	int tempX = firstParticle->x, tempY = firstParticle->y;
+	if ( rand() % 3 == 0 ) //1/3 Chance
+	{
+	    //Delete firstParticle
+	    firstParticle->x = oldXFirst;
+	    firstParticle->y = oldYFirst;
+	    deletePoint(firstParticle);
+	    firstParticle->hasMoved = FALSE;
+	}
+	else //2/3 Change of bouncing
+	{
+	    //Move the point back
+	    firstParticle->x = oldXFirst;
+	    firstParticle->y = oldYFirst;
+	    firstParticle->hasMoved = FALSE;
+	}
+			
+	break;
+    }
+    case 7: //Neutralizer - Acid
+    {
+	if (rand() % 3 == 0) //1/3 Chance
+	{
+	    //Delete secondParticle
+	    unSetPoint(secondParticle);
+	    secondParticle->hasMoved = FALSE;
+	}
+	else //2/3 Chance
+	{
+	    //Move the water back
+	    firstParticle->x = oldXFirst;
+	    firstParticle->y = oldYFirst;
+	    firstParticle->hasMoved = FALSE;
+	}
+
+	break;
+    }
+    case 8: //Salt - Water or Water - Salt or Salt - Ice or Ice - Salt or Salt-Water with any
+    {
+	//Move back and delete firstParticle
+	firstParticle->x = oldXFirst;
+	firstParticle->y = oldYFirst;
+	deletePoint(firstParticle);
+	firstParticle->hasMoved = FALSE;
+
+	//Change the element of secondParticle to Salt-water
+	setElement(secondParticle, elements[19]);
+	secondParticle->hasMoved = TRUE;
+
+	break;
+    }
+    case 9: //Salt-water - Plant or Plant - Salt-water
+    {
+	//Delete firstParticle
+	firstParticle->x = oldXFirst;
+	firstParticle->y = oldYFirst;
+	deletePoint(firstParticle);
+	firstParticle->hasMoved = FALSE;
+		    
+	//Change the element or secondParticle to Sand
+	setElement(secondParticle, elements[3]);
+	secondParticle->hasMoved = TRUE;
+	break;
+    }
+    case 10: //Water - Sand or Sand - Water
+    {
+	//Delete firstParticle
+	firstParticle->x = oldXFirst;
+	firstParticle->y = oldYFirst;
+	deletePoint(firstParticle);
+	firstParticle->hasMoved = FALSE;
+
+	//Change the element of secondParticle to Mud
+	setElement(secondParticle, elements[21]);
+	secondParticle->hasMoved = TRUE;
+	break;
+    }
+    }
+}
---
+++ TheElements-dev/jni/elementproperties.c
@@ -0,0 +1,202 @@
+/*
+ * elementproperties.c
+ * ----------------------------------------
+ * Contains all the arrays that define the element properties.
+ * When adding a new element, you need to add it to all of
+ * these arrays as well.
+ *
+ * Properties list:
+ * 		-red
+ * 		-green
+ * 		-blue
+ *		-fallVel
+ *		-density
+ *		-state
+ *		-special
+ *		-specialVal
+ *		-inertia
+ *
+ * Elements list:
+ * ----Special----
+ *		0 = Spawn
+ *		1 = Drag
+ *		2 = Eraser
+ * ----Normal----
+ *		3 = Sand
+ *		4 = Water
+ *		5 = Steam
+ *		6 = Ice
+ *		7 = Wall
+ *		8 = Destructible Wall
+ *		9 = Plant
+ *		10 = Fire
+ *		11 = Magma
+ *		12 = Stone
+ *		13 = Oil
+ *		14 = C4
+ *		15 = C4++
+ *		16 = Fuse
+ *		17 = Acid
+ *		18 = Salt
+ *		19 = Salt-Water
+ *		20 = Glass
+ *		21 = Mud
+ *		22 = Generator
+ *		23 = Coal
+ *		24 = Ant
+ *		25 = Gunpowder
+ *		26 = Mosquito
+ *		27 = Wood
+ *		28 = Termite
+ *		29 = Insect Killer
+ * ----Add Elements Above----
+ */
+
+//Include the global macros
+#include "elementproperties.h"
+
+// Define the names of the elements
+char* baseName[] = {
+		 // ----Special----
+		 "Spawn",		// 0 = Spawn
+		 "Drag",		// 1 = Drag
+		 "Eraser",		// 2 = Eraser
+		 // ----Normal----
+		 "Sand",		// 3 = Sand
+		 "Water",		// 4 = Water
+		 "Steam",		// 5 = Steam
+		 "Ice",			// 6 = Ice
+		 "Wall",		// 7 = Wall
+		 "Destructible Wall", // 8 = Destructible Wall
+		 "Plant",		// 9 = Plant
+		 "Fire",		// 10 = Fire
+		 "Magma",		// 11 = Magma
+		 "Stone",		// 12 = Stone
+		 "Oil",			// 13 = Oil
+		 "C4",			// 14 = C4
+		 "C4++",		// 15 = C4++
+		 "Fuse",		// 16 = Fuse
+		 "Acid",		// 17 = Acid
+		 "Salt",		// 18 = Salt
+		 "Salt-Water",	// 19 = Salt-Water
+		 "Glass",		// 20 = Glass
+		 "Mud",			// 21 = Mud
+		 "Generator",	// 22 = Generator
+		 "Coal",		// 23 = Coal
+		 "Bug",			// 24 = Bug
+		 "Gunpowder",	// 25 = Gunpowder
+		 "Mosquito",	// 26 = Mosquito
+		 "Wood",		// 27 = Wood
+		 "Termite",		// 28 = Termite
+		 "Insect Killer"// 29 = Insect Killer
+};
+
+//Define the color of the element
+unsigned char baseRed[] =   {255,   0,   0, 255,   0, 220, 200, 157, 176,   0, 255, 204, 100, 143, 255, 255, 135, 150, 255,  50, 200, 100, 255,  50,   0,  90,   0, 125, 200, 222};
+unsigned char baseGreen[] = {255,   0,   0, 213,   0, 220, 200, 157, 176, 255,   0,  51, 100,  80, 255, 211,  25, 255, 255, 100, 255,  85, 255,  50,  50,  90,  50,  50, 100, 200};
+unsigned char baseBlue[] =  {255,   0,   0,  85, 255, 225, 255, 157, 164,   0,   0,   0, 100,  80, 143, 115,  30,  50, 230, 200, 255,   0, 255,  50,   0,  90, 100,  10,  10, 0};
+
+//Define each base element's fall velocity
+signed char baseFallVel[] = {0, 0, 0, 1, 1, -1, 0, 0, 0, 0, -1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1,1};
+
+//Defines the density of each base element
+char baseDensity[] = {10, 10, 10, 10,  5,  1, 10, 10, 10, 10,  1,  7, 10,  3, 10, 10, 10,  4, 10,  4, 10,  9, 10, 10,  4, 10,  4,  4,  4, 8};
+
+//Defines the state of each base element (0 = Solid, 1 = Liquid, 2 = Gaseous)
+char baseState[] = {0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 2, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+
+/*
+ * Specials list:
+ *	   -1 = None;
+ *		1 = Spawn particles; Element
+ *		2 = Break; Threshold velocity
+ *		3 = Grow; Food element
+ *		4 = Burn;
+ *		5 = Explode; Explosiveness
+ *		6 = Life; 1/(Probability of deletion)
+ */
+//Defines the special for each base element
+
+signed char baseSpecial[][MAX_SPECIALS] =
+{
+		{SPECIAL_SPAWN, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_BREAK, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},  //Destructible Wall
+		{SPECIAL_GROW, SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_HEAT, SPECIAL_LIFE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Fire
+		{SPECIAL_HEAT, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Magma
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_BREAK, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE},
+		{SPECIAL_WANDER, SPECIAL_JUMP, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Bug
+		{SPECIAL_EXPLODE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Gunpowder
+		{SPECIAL_WANDER, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Mosquito
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Wood
+		{SPECIAL_WANDER, SPECIAL_TUNNEL, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE}, //Termite
+		{SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE, SPECIAL_NONE} //Insect Killer
+};
+
+
+//Defines the special value for each base element
+char baseSpecialValue[][MAX_SPECIALS] =
+{
+		{0,0,0,0,0,0}, //TODO: Add the element names next to all of these columns
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{4,0,0,0,0,0},
+		{20,5,0,0,0,0}, //Fire
+		{3,0,0,0,0,0}, //Magma
+		{0,0,0,0,0,0},
+		{7,3,0,0,0,0},
+		{10,0,0,0,0,0}, //C4
+		{20,0,0,0,0,0}, //C4++
+		{2,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{5,0,0,0,0,0},
+		{10,0,0,0,0,0}, //Bug
+		{20,0,0,0,0,0}, //Gunpowder
+		{10,0,0,0,0,0}, //Mosquito
+		{0,0,0,0,0,0}, //Wood
+		{5,27,0,0,0,0}, //Termite
+		{0,0,0,0,0,0} //Insect Killer
+};
+
+//Defines the inertia of each element
+char baseInertia[] = {255, 255, 255, 1, 1, 1, 255, 255, 255, 255, 1, 1, 3, 1, 255, 255, 255, 1, 1, 1, 255, 1, 255, 1, 1, 1, 1, 255, 1, 1};
+
+//Defines the initial temp of anything of that element that is spawned (0 = atmosphere)
+char baseStartingTemp[] = {0, 0, 0, 0, 100, 160, 1, 0, 0, 0, 255, 205, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+//Defines the highest and lowest temps that the element exists at
+char baseHighestTemp[] = {255, 255, 255, 154, 150, 255, 75, 255, 200, 170, 255, 255, 200, 125, 125, 125, 125, 150, 255, 150, 255, 150, 255, 225, 125, 125, 125, 150, 125, 255};
+char baseLowestTemp[] = {0, 0, 0, 0, 76, 151, 0, 0, 0, 0, 110, 201, 0, 0, 0, 0, 0, 0, 0, 60, 0, 0, 0, 0, 75, 0, 75, 0, 75, 0};
+
+//Defines the higher and lower elements to change to outside of the correct temp range
+char baseHigherElement[] = {0, 0, 0, 20, 5, 0, 4, 0, 11, 10, 0, 0, 11, 10, 10, 10, 10, 5, 0, 5, 0, 3, 0, 10, 10, 10, 10, 10, 10, 0};
+char baseLowerElement[] = {0, 0, 0, 0, 6, 4, 0, 0, 0, 0, 23, 12, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0};
