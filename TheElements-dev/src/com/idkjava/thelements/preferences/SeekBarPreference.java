4fc2bf6b6e679d93a3e12f625ccd87796db54d03
Gurtej Kanwar <gurtejkanwar@gmail.com>
2012-08-08 03:53:18 +0000

    Cleans up the code some.

--- TheElements-dev/src/com/idkjava/thelements/preferences/SeekBarPreference.java
+++ TheElements-dev/src/com/idkjava/thelements/preferences/SeekBarPreference.java
@@ -6,7 +6,6 @@ import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.preference.Preference;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
