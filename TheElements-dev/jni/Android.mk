a0f842536d1e87a58add9af4da12806346e5b2c1
Gurtej Kanwar <gurtej@mit.edu>
2013-07-10 00:16:06 -0700

    Adds hooks for android-ndk-profiler
    Save the state to get profiling data in /sdcard/gmon.out. Use adb
    pull to grab that file, then run gprof (the binary in the NDK) to
    get a human-readable format.

--- TheElements-dev/jni/Android.mk
+++ TheElements-dev/jni/Android.mk
@@ -1,11 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+$(call import-add-path, $(LOCAL_PATH)/../../)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := thelements
 
 LOCAL_CFLAGS := -DANDROID_NDK \
-                -DDISABLE_IMPORTGL
+                -DDISABLE_IMPORTGL \
+				-pg
+# compile with profiling
+LOCAL_STATIC_LIBRARIES := android-ndk-profiler
 
 LOCAL_SRC_FILES := \
     app-android.c \
@@ -23,3 +27,5 @@ LOCAL_SRC_FILES := \
 LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog
 
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android-ndk-profiler)
