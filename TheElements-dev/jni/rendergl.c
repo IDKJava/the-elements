3b1b27ba880ea8614877b0042292c43391ccf91a
Ryan Cheu <rcheu@quora.com>
2014-06-28 23:52:30 -0700

    OpenGL2 working

--- TheElements-dev/jni/rendergl.c
+++ TheElements-dev/jni/rendergl.c
@@ -26,178 +26,25 @@ unsigned int textureID;
 
 int mTextureCoordinateHandle;
 int mTextureUniformHandle;
+int mProjMatrixUniformHandle;
 
+float vertices[] = {0.0f,2.0f,
+		    2.0f,2.0f,
+                    2.0f,0.0f,
+                    0.0f,2.0f,
+                    2.0f,0.0f,
+                    0.0f,0.0f};
 
-//float vertices[] =
-//{0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f};
-
-float vertices[] = {0.0f,0.0f,
-                    0.5f,0.0f,
-                    0.5f,0.5f,
-                    0.0f,0.0f,
-                    0.5f,0.5f,
-                    0.0f,0.5f};
 float texture[] = {0.0f, 0.0f,
-		           1.0f, 0.0f,
-		           1.0f, 1.0f,
-		           0.0f, 0.0f,
-		           1.0f, 1.0f,
+		   1.0f, 0.0f,
+		   1.0f, 1.0f,
+		   0.0f, 0.0f,
+		   1.0f, 1.0f,
                    0.0f, 1.0f};
-//unsigned char indices[] =
-//{0, 1, 3, 0, 3, 2};
+
 int texWidth, texHeight, stupidTegra;
-/*
 
-void glInit()
-{
-	//Set some properties
-//glShadeModel(GL_COLOR_BUFFER_BIT);
-//glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
- 
-    glGenFramebuffers(1, &fb);
-    glGenRenderbuffers(1, &depthRb); // the depth buffer
-	//Generate the new texture
-	glGenTextures(1, &textureID);
-	//Bind the texture
-	glBindTexture(GL_TEXTURE_2D, textureID);
-
-	//Enable 2D texturing
-	//glEnable(GL_TEXTURE_2D);
-	//Disable depth testing
-	//glDisable(GL_DEPTH_TEST);
-
-	//Enable the vertex and coord arrays
-    //glEnableVertexAttribArrays();
-    //glEnableClientState(GL_VERTEX_ARRAY);
-	//glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-
-	//Set tex params
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	
-	//Set up texWidth and texHeight
-	texWidth = 1;
-	texHeight = 1;
-	while((texWidth = texWidth << 1) < (screenWidth-screenWidth%zoomFactor));
-	while((texHeight = texHeight << 1) < (screenHeight-screenHeight%zoomFactor));
-
-
-	//Allocate the dummy array
-	char* emptyPixels = malloc(3 * texWidth*texHeight * sizeof(char));
-	//Generate the tex image
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, emptyPixels);
-	//Free the dummy array
-	free(emptyPixels);
-
-	//Set the pointers
-	//glVertexPointer(2, GL_FLOAT, 0, vertices);
-	//glTexCoordPointer(2, GL_FLOAT, 0, texture);
-}
-
-void glRender()
-{
-    //Kamcord_BeginDraw();
-
-#if LOGGING
-    struct timeval time2;
-    struct timeval time3;
-    char buffer[20];
-    double useconds;
-
-    // TODO: First frame is wrong. We probably don't care...
-    gettimeofday(&time2, NULL);
-    useconds = (time2.tv_sec - time1.tv_sec)*1000000 + time2.tv_usec - time1.tv_usec;
-    snprintf(buffer, 20, "aFPS: %f", 1000000/useconds);
-    __android_log_write(ANDROID_LOG_INFO, "TheElements", buffer);
-    time1.tv_sec = time2.tv_sec;
-    time1.tv_usec = time2.tv_usec;
-#endif
-
-
-
-	if(dimensionsChanged)
-	{
-		__android_log_write( ANDROID_LOG_INFO, "TheElements", "dimensions changed" );
-		vertices[2] = (float) screenWidth;
-		vertices[5] = (float) screenHeight;
-		vertices[6] = (float) screenWidth;
-		vertices[7] = (float) screenHeight;
-
-		texture[2] = (float) workWidth/texWidth;
-		texture[5] = (float) workHeight/texHeight;
-		texture[6] = (float) workWidth/texWidth;
-		texture[7] = (float) workHeight/texHeight;
-
-		dimensionsChanged = FALSE;
-		zoomChanged = FALSE;
-		//__android_log_write(ANDROID_LOG_INFO, "MainActivity", "Dimensions changed");
-
-	}
-	else if(zoomChanged)
-	{
-		texture[2] = (float) workWidth/texWidth;
-		texture[5] = (float) workHeight/texHeight;
-		texture[6] = (float) workWidth/texWidth;
-		texture[7] = (float) workHeight/texHeight;
-
-		zoomChanged = FALSE;
-		//__android_log_write(ANDROID_LOG_INFO, "MainActivity", "zoom changed");
-	}
-
-    //glMatrixMode(GL_PROJECTION);
-    //glLoadIdentity();
-    float modViewWidth = viewWidth/2 * zoomScale;
-    float modViewHeight = viewHeight/2 * zoomScale;
-    float left = centerX - modViewWidth;
-    float right = centerX + modViewWidth;
-    float top = centerY - modViewHeight;
-    float bottom = centerY + modViewHeight;
-    if (!flipped)
-    {
-        glViewport(left,right, bottom, top);
-        //glOrthof(left, right, bottom, top, -1.0f, 1.0f); //--Device
-    }
-    else
-    {
-        //glOrthof(left, right, bottom, -1*top, -1.0f, 1.0f); //--Emulator
-    }
-
-    glBindFramebuffer(GL_FRAMEBUFFER, fb);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, &textureID, 0);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, &depthRb);
-
-    //int status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    //if (status != GL_FRAMEBUFFER_COMPLETE)
-    //{
-        //This is an error
-        //return;
-//}
-
-
-    glClearColor(1,0,1,1);
-    glClear(GL_COLOR_BUFFER_BIT );
-
-	//Sub the work portion of the tex(~.025s -- Droid)
-#if LOGGING
-    gettimeofday(&time2, NULL);
-#endif
-glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, stupidTegra, workHeight, GL_RGB, GL_UNSIGNED_BYTE, colorsFrameBuffer);
-#if LOGGING
-    gettimeofday(&time3, NULL);
-    useconds = (time3.tv_sec - time2.tv_sec)*1000000 + time3.tv_usec - time2.tv_usec;
-    snprintf(buffer, 20, "tFPS: %f", 1000000/useconds);
-    __android_log_write(ANDROID_LOG_INFO, "TheElements", buffer);
-#endif
-
-    //Actually draw the rectangle with the text on it (~.015s -- Droid)
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
-    //Kamcord_EndDraw();
-}
-
-*/
-
-#define  LOG_TAG    "libgl2jni"
+#define  LOG_TAG    "NativeGL"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
@@ -209,51 +56,51 @@ static void printGLString(const char *name, GLenum s) {
 static void checkGlError(const char* op) {
     GLint error = glGetError();
     for (; error; error = glGetError()) {
-        LOGI("after %s() glError (0x%x)\n", op, error);
+	LOGI("after %s() glError (0x%x)\n", op, error);
     }
 }
 
-static const char gVertexShader[] = 
+static const char gVertexShader[] =
     "attribute vec4 vPosition;\n"
     "attribute vec2 a_TexCoordinate;\n"
     "varying vec2 v_TexCoordinate;\n"
+    "uniform mat4 u_MVPMatrix;\n"
     "void main() {\n"
     "  v_TexCoordinate = a_TexCoordinate;\n"
     "  gl_Position = vPosition;\n"
+    "  gl_Position *= u_MVPMatrix;\n"
     "}\n";
 
-static const char gFragmentShader[] = 
+static const char gFragmentShader[] =
     "precision mediump float;\n"
     "varying vec2 v_TexCoordinate;\n"
     "uniform sampler2D u_Texture;\n"
     "void main() {\n"
-    "  gl_FragColor = texture2D(u_Texture, v_TexCoordinate);"
+    "  gl_FragColor = texture2D(u_Texture, v_TexCoordinate);\n"
     "}\n";
-
-//    "  gl_FragColor = texture2D(u_Texture, v_TexCoordinate);\n"
 
 GLuint loadShader(GLenum shaderType, const char* pSource) {
     GLuint shader = glCreateShader(shaderType);
     if (shader) {
-        glShaderSource(shader, 1, &pSource, NULL);
-        glCompileShader(shader);
-        GLint compiled = 0;
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-        if (!compiled) {
-            GLint infoLen = 0;
-            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-            if (infoLen) {
-                char* buf = (char*) malloc(infoLen);
-                if (buf) {
-                    glGetShaderInfoLog(shader, infoLen, NULL, buf);
-                    LOGE("Could not compile shader %d:\n%s\n",
-                            shaderType, buf);
-                    free(buf);
-                }
-                glDeleteShader(shader);
-                shader = 0;
-            }
-        }
+	glShaderSource(shader, 1, &pSource, NULL);
+	glCompileShader(shader);
+	GLint compiled = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+	    GLint infoLen = 0;
+	    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+	    if (infoLen) {
+		char* buf = (char*) malloc(infoLen);
+		if (buf) {
+		    glGetShaderInfoLog(shader, infoLen, NULL, buf);
+		    LOGE("Could not compile shader %d:\n%s\n",
+			 shaderType, buf);
+		    free(buf);
+		}
+		glDeleteShader(shader);
+		shader = 0;
+	    }
+	}
     }
     return shader;
 }
@@ -261,43 +108,68 @@ GLuint loadShader(GLenum shaderType, const char* pSource) {
 GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
     GLuint vertexShader = loadShader(GL_VERTEX_SHADER, pVertexSource);
     if (!vertexShader) {
-        return 0;
+	return 0;
     }
 
     GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, pFragmentSource);
     if (!pixelShader) {
-        return 0;
+	return 0;
     }
 
     GLuint program = glCreateProgram();
     if (program) {
-        glAttachShader(program, vertexShader);
-        checkGlError("glAttachShader");
-        glAttachShader(program, pixelShader);
-        checkGlError("glAttachShader");
-        glLinkProgram(program);
-        GLint linkStatus = GL_FALSE;
-        glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
-        if (linkStatus != GL_TRUE) {
-            GLint bufLength = 0;
-            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
-            if (bufLength) {
-                char* buf = (char*) malloc(bufLength);
-                if (buf) {
-                    glGetProgramInfoLog(program, bufLength, NULL, buf);
-                    LOGE("Could not link program:\n%s\n", buf);
-                    free(buf);
-                }
-            }
-            glDeleteProgram(program);
-            program = 0;
-        }
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, pixelShader);
+	glLinkProgram(program);
+	GLint linkStatus = GL_FALSE;
+	glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+	if (linkStatus != GL_TRUE) {
+	    GLint bufLength = 0;
+	    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
+	    if (bufLength) {
+		char* buf = (char*) malloc(bufLength);
+		if (buf) {
+		    glGetProgramInfoLog(program, bufLength, NULL, buf);
+		    LOGE("Could not link program:\n%s\n", buf);
+		    free(buf);
+		}
+	    }
+	    glDeleteProgram(program);
+	    program = 0;
+	}
 
-        mTextureCoordinateHandle = glGetAttribLocation(program, "a_TexCoordinate");
-        mTextureUniformHandle = glGetUniformLocation(program, "u_Texture");
+	mTextureCoordinateHandle = glGetAttribLocation(program, "a_TexCoordinate");
+	mTextureUniformHandle = glGetUniformLocation(program, "u_Texture");
+	mProjMatrixUniformHandle = glGetUniformLocation(program, "u_MVPMatrix");
     }
     return program;
 }
+
+
+//Makes an orthographic projection matrix
+void setOthographicMat(float l, float r, float t, float b, float n, float f,  float mat[])
+{
+    mat[0] = 2.0 / (r - l);
+    mat[1] = 0;
+    mat[2] = 0;
+    mat[3] = -(r + l) / (r - l);
+
+    mat[4] = 0;
+    mat[5] = 2.0 / (t - b);
+    mat[6] = 0;
+    mat[7] = -(t + b) / (t - b);
+
+    mat[8] = 0;
+    mat[9] = 0;
+    mat[10] = - 2.0 / (f - n);
+    mat[11] = - (f+n) / (f - n);
+
+    mat[12] = 0;
+    mat[13] = 0;
+    mat[14] = 0;
+    mat[15] = 1;
+}
+
 
 GLuint gProgram;
 GLuint gvPositionHandle;
@@ -311,110 +183,92 @@ void glInit() {
     LOGI("setupGraphics(%d, %d)", screenWidth, screenHeight);
     gProgram = createProgram(gVertexShader, gFragmentShader);
     if (!gProgram) {
-        LOGE("Could not create program.");
-        return;
+	LOGE("Could not create program.");
+	return;
     }
     gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
-    checkGlError("glGetAttribLocation");
-    LOGI("glGetAttribLocation(\"vPosition\") = %d\n",
-            gvPositionHandle);
 
     glViewport(0, 0, screenWidth, screenHeight);
-    checkGlError("glViewport");
-
     //Generate the new texture
-	glGenTextures(1, &textureID);
-    checkGlError("glGenTextures");
-	//Bind the texture
-	glBindTexture(GL_TEXTURE_2D, textureID);
+    glGenTextures(1, &textureID);
+    checkGlError("glTexParam");
+    //Bind the texture
+    glBindTexture(GL_TEXTURE_2D, textureID);
     checkGlError("glBindTexture");
 
     //Set tex params
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     checkGlError("glTexParam");
-	
-	//Set up texWidth and texHeight
-	texWidth = 1;
-	texHeight = 1;
-	while((texWidth = texWidth << 1) < (screenWidth-screenWidth%zoomFactor));
-	while((texHeight = texHeight << 1) < (screenHeight-screenHeight%zoomFactor));
+
+    //Set up texWidth and texHeight
+    texWidth = 1;
+    texHeight = 1;
+    while((texWidth = texWidth << 1) < (screenWidth-screenWidth%zoomFactor));
+    while((texHeight = texHeight << 1) < (screenHeight-screenHeight%zoomFactor));
 
 
-	//Allocate the dummy array
-	char* emptyPixels = malloc(3 * texWidth*texHeight * sizeof(char));
-	//Generate the tex image
-    //LOGI("Running the teximage2d");
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, emptyPixels);
+    //Allocate the dummy array
+    char* emptyPixels = malloc(3 * texWidth*texHeight * sizeof(char));
+    //Generate the tex image
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, emptyPixels);
     checkGlError("glTexImage2D");
-	//Free the dummy array
-	free(emptyPixels);
+    //Free the dummy array
+    free(emptyPixels);
     return;
 }
 
-//const GLfloat gTriangleVertices[] = { 0.0f, 0.5f, -0.5f, -0.5f,
-//0.5f, -0.5f };
+
 
 void glRender() {
-    static float grey;
-    grey += 0.01f;
-    if (grey > 1.0f) {
-        grey = 0.0f;
-    }
 
     // Update dimensions
-	texture[3] = (float) workWidth/texWidth;
-	texture[5] = (float) workWidth/texWidth;
-	texture[9] = (float) workWidth/texWidth;
-	texture[6] = (float) workHeight/texHeight;
-	texture[10] = (float) workHeight/texHeight;
-	texture[12] = (float) workHeight/texHeight;
+    texture[2] = (float) workWidth/texWidth;
+    texture[4] = (float) workWidth/texWidth;
+    texture[8] = (float) workWidth/texWidth;
+    texture[5] = (float) workHeight/texHeight;
+    texture[9] = (float) workHeight/texHeight;
+    texture[11] = (float) workHeight/texHeight;
 
-    glClearColor(grey, grey, grey, 1.0f);
-    checkGlError("glClearColor");
     glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    checkGlError("glClear");
 
     glUseProgram(gProgram);
-    checkGlError("glUseProgram");
 
-    checkGlError("gl binding");
-    
-    //char* emptyPixels = malloc(3 * stupidTegra*workHeight * sizeof(char));
-	//Generate the tex image
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, textureID);
 
-	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, stupidTegra, workHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, emptyPixels);
-
-	//Free the dummy array
-    //free(emptyPixels);
-    //LOGI("Running the teximage2d");
-    LOGI("Tex width: %d, tex height: %d, screen width: %d, screen height: %d",
-    		texWidth, texHeight, screenWidth, screenHeight);
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, stupidTegra, workHeight, GL_RGB, GL_UNSIGNED_BYTE, colorsFrameBuffer);
-    checkGlError("glTexImage2D");
-    //LOGI("Running the tex sub image");
-    checkGlError("texsubimage2d");
- 
-    // Bind the texture to this unit.
 
- 
     // Tell the texture uniform sampler to use this texture in the shader by binding to texture unit 0.
     glUniform1i(mTextureUniformHandle, 0);
 
     glEnableVertexAttribArray(gvPositionHandle);
-    //LOGI("Running enable");
-    glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-    checkGlError("glVertexAttribPointer");
-    glEnableVertexAttribArray(mTextureCoordinateHandle);
-    //LOGI("Running enable2");
-    glVertexAttribPointer(mTextureCoordinateHandle, 2, GL_FLOAT, false, 0, texture);
-    
-    checkGlError("glEnableVertexAttribArray");
-    glDrawArrays(GL_TRIANGLES, 0, 6);
 
-    checkGlError("glDrawArrays");
+    glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_TRUE, 0, vertices);
+    glEnableVertexAttribArray(mTextureCoordinateHandle);
+
+    glVertexAttribPointer(mTextureCoordinateHandle, 2, GL_FLOAT, GL_TRUE, 0, texture);
+
+
+    //Set up the projection matrix for zoom
+    float proj[16];
+    float yOffset = centerY - (screenHeight/2);
+    float modCenterY = (screenHeight/2) - yOffset;
+
+    float modViewWidth = viewWidth/2 * zoomScale;
+    float modViewHeight = viewHeight/2 * zoomScale;
+    float left = centerX - modViewWidth;
+    float right = centerX + modViewWidth;
+    float top = modCenterY + modViewHeight;
+    float bottom = modCenterY - modViewHeight;
+
+
+    //TODO: Figure out why everything here is multiplied by 0.5 (probaly should reverse the verticies being 2.0 o something)
+    setOthographicMat(left/(0.5*screenWidth), right/(0.5*screenWidth), top/(0.5*screenHeight), bottom/(0.5*screenHeight), 1, -1, proj);
+
+    glUniformMatrix4fv(mProjMatrixUniformHandle, 1, GL_FALSE, &proj[0]);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 
