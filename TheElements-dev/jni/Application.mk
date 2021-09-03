bdec6e04bda28b8812797131827ea0c58b130863
Gurtej Kanwar <gurtej@mit.edu>
2021-09-04 00:10:16 +0200

    Adds missing ASan files, update to final release code for v116 (7.0.2)

--- TheElements-dev/jni/Application.mk
+++ TheElements-dev/jni/Application.mk
@@ -2,5 +2,7 @@ APP_PLATFORM := android-29
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_STL := c++_shared
 # For ASan
-APP_CFLAGS := -fsanitize=address -fno-omit-frame-pointer
-APP_LDFLAGS := -fsanitize=address
+ifeq ($(APP_OPTIM),debug)
+    APP_CFLAGS := -fsanitize=address -fno-omit-frame-pointer
+    APP_LDFLAGS := -fsanitize=address
+endif
